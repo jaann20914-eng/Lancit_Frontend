fb3f951a77b1e1feda8a74616d0606a406c6ac64
Yoon Young Eun <dms90904@gmail.com>
2026-06-20 12:15:31 +0900

    feat: 회사 공고 관리

--- src/widgets/sidebar/AppSidebar.vue
+++ src/widgets/sidebar/AppSidebar.vue
@@ -88,7 +88,7 @@
             <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/>
             <polyline points="9 22 9 12 15 12 15 22"/>
           </svg>
-          공고문
+          공고 관리
         </RouterLink>
       </template>
     </nav>
